6f2b95e343e741f0d4529a112095f9cc80dd493d
Miguel Ángel Barrera <mbmdevelop@gmail.com>
2023-09-21 12:12:30 +0200

    wip

--- RecordingVideoMaui/CamViewModel.cs
+++ RecordingVideoMaui/CamViewModel.cs
@@ -1,14 +1,12 @@
-﻿using Camera.MAUI.ZXingHelper;
-using Camera.MAUI;
+﻿using Camera.MAUI;
 using CommunityToolkit.Maui.Views;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using ZXing;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace RecordingVideoMaui
 {
-    public class CamViewModel : ObservableObject
+    public partial class CamViewModel : ObservableObject
     {
         private CameraInfo camera = null;
         public CameraInfo Camera
@@ -19,9 +17,7 @@ namespace RecordingVideoMaui
                 camera = value;
                 OnPropertyChanged(nameof(Camera));
                 AutoStartPreview = false;
-                OnPropertyChanged(nameof(AutoStartPreview));
                 AutoStartPreview = true;
-                OnPropertyChanged(nameof(AutoStartPreview));
             }
         }
         private ObservableCollection<CameraInfo> cameras = new();
@@ -71,8 +67,12 @@ namespace RecordingVideoMaui
             }
         }
         public MediaSource VideoSource { get; set; }
-        public bool AutoStartPreview { get; set; } = false;
-        public bool AutoStartRecording { get; set; } = false;
+
+        [ObservableProperty]
+        public bool autoStartPreview = false;
+
+        [ObservableProperty]
+        public bool autoStartRecording = false;
      
         private bool takeSnapshot = false;
         public bool TakeSnapshot
@@ -97,53 +97,46 @@ namespace RecordingVideoMaui
                 }
             }
         }
-        public Command StartCamera { get; set; }
-        public Command StopCamera { get; set; }
         public Command TakeSnapshotCmd { get; set; }
         public string RecordingFile { get; set; }
-        public Command StartRecording { get; set; }
-        public Command StopRecording { get; set; }
-
         public CamViewModel()
         {
-            StartCamera = new Command(() =>
-            {
-                AutoStartPreview = true;
-                OnPropertyChanged(nameof(AutoStartPreview));
-            });
-            StopCamera = new Command(() =>
-            {
-                AutoStartPreview = false;
-                OnPropertyChanged(nameof(AutoStartPreview));
-            });
             TakeSnapshotCmd = new Command(() =>
             {
                 TakeSnapshot = false;
                 TakeSnapshot = true;
             });
 #if IOS
-        RecordingFile = Path.Combine(FileSystem.Current.CacheDirectory, "Video.mov");
+            RecordingFile = Path.Combine(FileSystem.Current.CacheDirectory, "Video.mov");
 #else
             RecordingFile = Path.Combine(FileSystem.Current.AppDataDirectory, "Video.mp4");
 #endif
             OnPropertyChanged(nameof(RecordingFile));
-            StartRecording = new Command(() =>
-            {
-                AutoStartRecording = true;
-                OnPropertyChanged(nameof(AutoStartRecording));
-            });
-            StopRecording = new Command(() =>
-            {
-                AutoStartRecording = false;
-                OnPropertyChanged(nameof(AutoStartRecording));
-                VideoSource = MediaSource.FromFile(RecordingFile);
-                OnPropertyChanged(nameof(VideoSource));
-            });
-            OnPropertyChanged(nameof(StartCamera));
-            OnPropertyChanged(nameof(StopCamera));
             OnPropertyChanged(nameof(TakeSnapshotCmd));
             OnPropertyChanged(nameof(StartRecording));
             OnPropertyChanged(nameof(StopRecording));
+
+            
+        }
+
+        [RelayCommand]
+        public void StartRecording()
+        {
+           AutoStartRecording = true;
+        }
+
+        [RelayCommand]
+        public void StopRecording()
+        {
+           AutoStartRecording = false;
+           VideoSource = MediaSource.FromFile(RecordingFile);
+           OnPropertyChanged(nameof(VideoSource));
+        }
+
+        [RelayCommand]
+        public void Rec()
+        {
+           AutoStartPreview = true;
         }
     }
 }
